7277ae50c053aba757fac14d45ef64da69a6f77e
Erik Unger <erik@erikunger.com>
2014-04-17 21:39:29 +0200

    added IsZero

--- reflect.go
+++ reflect.go
@@ -173,3 +173,39 @@ func InterfaceSlice(slice interface{}) []interface{} {
 	}
 	return result
 }
+
+func IsZero(value interface{}) bool {
+	if value == nil {
+		return true
+	}
+
+	v := reflect.ValueOf(value)
+	// if IsFakeZero(v) {
+	// 	return true
+	// }
+
+	switch v.Kind() {
+	case reflect.String:
+		return v.Len() == 0
+
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Uint() == 0
+
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+
+	case reflect.Bool:
+		return v.Bool() == false
+
+	case reflect.Ptr, reflect.Chan, reflect.Func, reflect.Interface, reflect.Slice, reflect.Map:
+		return v.IsNil()
+
+	case reflect.Struct:
+		return reflect.DeepEqual(value, reflect.Zero(v.Type()).Interface())
+	}
+
+	panic(fmt.Errorf("Unknown value kind %T", value))
+}
