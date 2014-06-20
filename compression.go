7bcc5135f7dbfb0d766f71e262e03fb59550056d
Erik Unger <erik@erikunger.com>
2014-06-20 15:02:16 +0200

    DeflatePool and GzipPool use Go 1.3 sync.Pool internally

--- compression.go
+++ compression.go
@@ -15,30 +15,20 @@ var (
 // DeflatePool manages a pool of flate.Writer
 // flate.NewWriter allocates a lot of memory, so if flate.Writer
 // are needed frequently, it's more efficient to use a pool of them.
-// The pool is unlimited and grows on demand.
-// So if 100 writers are requested before
-// any is returned, then 100 writers will be created.
+// The pool uses sync.Pool internally.
 type DeflatePool struct {
-	writers []*flate.Writer
-	mutex   sync.Mutex
+	pool sync.Pool
 }
 
 // GetWriter returns flate.Writer from the pool, or creates a new one
 // with flate.BestCompression if the pool is empty.
 func (pool *DeflatePool) GetWriter(dst io.Writer) (writer *flate.Writer) {
-	pool.mutex.Lock()
-
-	count := len(pool.writers)
-	if count == 0 {
-		writer, _ = flate.NewWriter(dst, flate.BestCompression)
+	if w := pool.pool.Get(); w != nil {
+		writer = w.(*flate.Writer)
+		writer.Reset(dst)
 	} else {
-		writer = pool.writers[count-1]
-		pool.writers = pool.writers[:count-1]
-		defer writer.Reset(dst) // do it after pool.mutex.Unlock()
+		writer, _ = flate.NewWriter(dst, flate.BestCompression)
 	}
-
-	pool.mutex.Unlock()
-
 	return writer
 }
 
@@ -48,43 +38,24 @@ func (pool *DeflatePool) GetWriter(dst io.Writer) (writer *flate.Writer) {
 // it to the pool.
 func (pool *DeflatePool) ReturnWriter(writer *flate.Writer) {
 	writer.Close()
-	pool.mutex.Lock()
-	pool.writers = append(pool.writers, writer)
-	pool.mutex.Unlock()
-}
-
-// Clean removes all flate.Writer from the pool.
-func (pool *DeflatePool) Clean() {
-	pool.mutex.Lock()
-	pool.writers = pool.writers[0:0]
-	pool.mutex.Unlock()
+	pool.pool.Put(writer)
 }
 
 // GzipPool manages a pool of gzip.Writer.
-// The pool is unlimited and grows on demand.
-// So if 100 writers are requested before
-// any is returned, then 100 writers will be created.
+// The pool uses sync.Pool internally.
 type GzipPool struct {
-	writers []*gzip.Writer
-	mutex   sync.Mutex
+	pool sync.Pool
 }
 
 // GetWriter returns gzip.Writer from the pool, or creates a new one
 // with gzip.BestCompression if the pool is empty.
 func (pool *GzipPool) GetWriter(dst io.Writer) (writer *gzip.Writer) {
-	pool.mutex.Lock()
-
-	count := len(pool.writers)
-	if count == 0 {
-		writer, _ = gzip.NewWriterLevel(dst, gzip.BestCompression)
+	if w := pool.pool.Get(); w != nil {
+		writer = w.(*gzip.Writer)
+		writer.Reset(dst)
 	} else {
-		writer = pool.writers[count-1]
-		pool.writers = pool.writers[:count-1]
-		defer writer.Reset(dst) // do it after pool.mutex.Unlock()
+		writer, _ = gzip.NewWriterLevel(dst, gzip.BestCompression)
 	}
-
-	pool.mutex.Unlock()
-
 	return writer
 }
 
@@ -94,14 +65,5 @@ func (pool *GzipPool) GetWriter(dst io.Writer) (writer *gzip.Writer) {
 // it to the pool.
 func (pool *GzipPool) ReturnWriter(writer *gzip.Writer) {
 	writer.Close()
-	pool.mutex.Lock()
-	pool.writers = append(pool.writers, writer)
-	pool.mutex.Unlock()
-}
-
-// Clean removes all gzip.Writer from the pool.
-func (pool *GzipPool) Clean() {
-	pool.mutex.Lock()
-	pool.writers = pool.writers[0:0]
-	pool.mutex.Unlock()
+	pool.pool.Put(writer)
 }
