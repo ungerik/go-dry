072617094d4ea2b1a5f71d0374e3b8ced33cac9d
Erik Unger <erik@erikunger.com>
2016-03-28 06:56:18 +0200

    added RandomHexString and RandomHEXString

--- rand.go
+++ rand.go
@@ -1,10 +1,33 @@
 package dry
 
 import (
-	"math/rand"
+	cryptoRand "crypto/rand"
+	"fmt"
+	mathRand "math/rand"
 	"time"
 )
 
+// RandSeedWithTime calls rand.Seed() with the current time.
 func RandSeedWithTime() {
-	rand.Seed(time.Now().UTC().UnixNano())
+	mathRand.Seed(time.Now().UTC().UnixNano())
+}
+
+// RandomHexString returns a random lower case hex string with length.
+func RandomHexString(length int) string {
+	buffer := make([]byte, length/2)
+	_, err := cryptoRand.Read(buffer)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%x", buffer)
+}
+
+// RandomHEXString returns a random upper case hex string with length.
+func RandomHEXString(length int) string {
+	buffer := make([]byte, length/2)
+	_, err := cryptoRand.Read(buffer)
+	if err != nil {
+		return ""
+	}
+	return fmt.Sprintf("%X", buffer)
 }
